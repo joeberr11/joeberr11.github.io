c9d70db281ac60eedf1f1ea6d5c4b7ec2ddc7c9d
joeberr11 <94065932+joeberr11@users.noreply.github.com>
2021-11-10 20:39:27 -0500

    Add files via upload

---
+++ amuse_scripts.js
@@ -0,0 +1,9 @@
+//Due Novemeber 13th
+//Joseph Berrigan
+//Assign #4
+function rideInfo(rideID, rideName){
+  alert("You clicked on the ride " + rideName + " with the ID of " + rideID);
+}
+function orderTics(){
+  alert("Your total is $35.");
+}
